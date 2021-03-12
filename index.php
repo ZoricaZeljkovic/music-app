4061ed7a41dcff0c60ef0708a6fbfb30ba941078
Zorica Zeljkovic <zorica.zeljkovic1977@gmail.com>
2021-03-12 17:18:46 +0100

    header html

--- index.php
+++ index.php
@@ -1,33 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>MusicApp</title>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet"> 
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-<body>
-   <!-- nav -->
-   <nav>
-       <a href="index.php"><span>Music</span>App</a>
-       <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="how-it-works.php">How it works</a></li>
-          <li><a href="subscription.php">Subscription</a></li>
-       </ul>
-   </nav>
-   <!-- footer -->
-   <footer>
-       <a href="index.php" class="logo"><span>Music</span>App</a>
-       <p>&copy; 2021. All right reserved</p>      
-       <a href=""><i class="fab fa-facebook-square"></i></a>
-       <a href=""><i class="fab fa-instagram"></i></a>
-       <a href=""><i class="fab fa-soundcloud"></i></a>
-       <a href=""><i class="fab fa-youtube"></i></a>
-   </footer>
+<?php include "partials/header.php"; ?>
 
-    
-</body>
-</html>
+<!-- header -->
+<header class="mainHeader">
+    <div>
+        <h1><span>Music</span> for everyone</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod libero dolorum voluptatum eos nisi delectus
+            est temporibus blanditiis reiciendis magnam nulla quas veniam, et impedit. Doloribus alias sunt sit est.</p>
+        <button>Start free trial</button>
+    </div>
+    <div>
+        <img src="img/header.png" alt="">
+    </div>
+</header>
+
+<?php include "partials/footer.php"; ?>
