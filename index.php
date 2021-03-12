89f97994ee60ccec007fb341f18553ce98c215ee
Zorica Zeljkovic <zorica.zeljkovic1977@gmail.com>
2021-03-12 11:21:29 +0100

    footer css

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
        <a href="index.php" class="logo"><span>Music</span>App</a>
        <p>&copy; 2021. All right reserved</p>      
        <a href=""><i class="fab fa-facebook-square"></i></a>
-       <a href=""><i class="fab fa-facebook-instagram"></i></a>
+       <a href=""><i class="fab fa-instagram"></i></a>
        <a href=""><i class="fab fa-soundcloud"></i></a>
        <a href=""><i class="fab fa-youtube"></i></a>
    </footer>
