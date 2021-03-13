916eed030568f9d10830f1385d4abe62279fb842
Zorica Zeljkovic <zorica.zeljkovic1977@gmail.com>
2021-03-13 17:36:00 +0100

    concept html

--- subscription.php
+++ subscription.php
@@ -1,5 +1,6 @@
 <?php include "partials/header.php"; ?>
 
+<!-- subscription -->
 <section class="subscription">
     <article class="container">
         <div>
@@ -19,6 +20,37 @@
                 <li>High quality</li>
                 <li>Shuffle play</li>
             </ul>
+        </div>
+    </article>
+</section>
+
+<!-- concept -->
+<section class="concept container">
+    <article>
+        <h2>Our Concept & artists</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis velit excepturi ipsam quidem nisi
+            officiis veniam, molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus iusto
+            sapiente laboriosam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis velit excepturi ipsam quidem
+            nisi officiis veniam, molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus iusto
+            sapiente laboriosam.</p>
+    </article>
+    <article>
+        <div>
+            <img src="img/subscription1.jpg" alt="subscription1">
+            <h3>No ad interruptions</h3>
+        </div>
+        <div>
+            <img src="img/subscription2.jpg" alt="subscription2">
+            <h3>High Quality</h3>
+        </div>
+        <div>
+            <img src="img/subscription3.jpg" alt="subscription3">
+            <h3>Listen Offline</h3>
+        </div>
+        <div>
+            <img src="img/subscription4.jpg" alt="subscription4">
+            <h3>Download Music</h3>
         </div>
     </article>
 </section>
