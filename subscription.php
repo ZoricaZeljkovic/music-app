51a2b5e74d316dd562d00e1bbac59a823787ca25
Zorica Zeljkovic <zorica.zeljkovic1977@gmail.com>
2021-03-13 17:06:57 +0100

    subscription html

--- subscription.php
+++ subscription.php
@@ -1,5 +1,26 @@
 <?php include "partials/header.php"; ?>
 
-  <h1 style="font-size: 9rem; text-align: center">Subscription</h1>
-  
+<section class="subscription">
+    <article class="container">
+        <div>
+            <h2>Subscription from $15/month</h2>
+            <h4>Start a free trial now</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla repellendus pariatur delectus nihil
+                quibusdam veritatis quae officiis exercitationem ab maxime! Quaerat explicabo exercitationem ipsam odit
+                sit omnis tempora illum quibusdam.</p>
+            <button>Start free trial</button>
+        </div>
+        <div>
+            <ul>
+                <li>Play any track</li>
+                <li>Listen offline</li>
+                <li>No add interruptions</li>
+                <li>Unlimited skips</li>
+                <li>High quality</li>
+                <li>Shuffle play</li>
+            </ul>
+        </div>
+    </article>
+</section>
+
 <?php include "partials/footer.php"; ?>
